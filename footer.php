9ae7cefdf4a6f978f348e88ea68748b1c610cdb5
KrixsJDK <caab.1315250025@gmail.com>
2023-09-24 16:16:25 -0500

    aws

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer style="position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: red;
+   color: white;
+   text-align: center;">
+    <h2><a href="https://www.uleam.edu.ec/" style="text-decoration: none; color:white;">BIBLIOTECA ULEAM</a></h2>
+</footer>
